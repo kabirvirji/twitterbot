e163f386dde18a1bf2a2069610227d659667bddf
Alexander Jurcau <jurcaua@gmail.com>
2016-04-02 17:10:36 -0400

    Created the looping function and added to the word lists

--- bot.py
+++ bot.py
@@ -1,5 +1,16 @@
 import tweepy
 import random
+import time
+
+firstnouns = ("Donald Trump", "Hilary Clinton", "Bernie Sanders", "Ted Cruz", "Person of colour", \
+                "Anonymous", "A Fox News Reporter")
+verbs = ("subjected", "hits", "jumps on", "drives", "barfs on", "burns", "practices") 
+secondnoun = ("themself", "a confederate flag", "racism", "a female", "an eagle", "Donald Trump", \
+                "Hilary Clinton", "Anonymous", "a Donald Trump protester")
+adv = ("crazily.", "stupidly.", "foolishly.", "fantastically.", "occasionally.", \
+                "for probably a good amount of the day.", "arrogantly", "awkwardly", "compassionately", \
+                "cunningly", "emotionally", "greedily", "kindheartedly", "lovingly", "occasionally", \
+                "passionately", "respectfully", "ruthlessly", "subtly", "wiggly")
 
 class TwitterAPI:
     def __init__(self):
@@ -14,31 +25,60 @@ class TwitterAPI:
     def tweet(self, message):
         self.api.update_status(status=message)
 
-firstnouns = ("Donald Trump", "Hilary Clinton", "Bernie Sanders", "Ted Cruz", "Person of colour")
-verbs = ("subjected", "hits a", "jumps on", "drives", "barfs on", "burns", "practices") 
-secondnoun = ("themself", "a confederate flag", "racism", "a female", "monkey", "Donald Trump", "Hilary Clinton")
-adv = ("crazily.", "stupidly.", "foolishly.", "fantastically.", "occasionally.", "for probably a good amount of the day.")
+    def trumpTrumpXTimes (self, x):
+        if x == '-1':
+            while (true):
+                random_sentance = '@realDonaldTrump '
 
-random_sentance = '@realDonaldTrump '
+                # add the first noun which is always a person
+                num = random.randrange(0, len(firstnouns))
+                random_sentance += firstnouns[num] + ' '
+                # what is this person doing?
+                num = random.randrange(0,len(verbs))
+                random_sentance += verbs[num] + ' '
+                # and to what/who is this person doing that thing?
+                num = random.randrange(0,len(secondnoun))
+                random_sentance += secondnoun[num] + ' '
+                ifAdv = random.randrange(0,2)
+                # add an adverb only sometimes
+                if ifAdv == 0:
+                    num = random.randrange(0,len(adv))
+                    random_sentance += adv[num]
+                else:
+                    random_sentance += '.'
 
-# add the first noun which is always a person
-num = random.randrange(0, len(firstnouns))
-random_sentance += firstnouns[num] + ' '
-# what is this person doing?
-num = random.randrange(0,len(verbs))
-random_sentance += verbs[num] + ' '
-# and to what/who is this person doing that thing?
-num = random.randrange(0,len(secondnoun))
-random_sentance += secondnoun[num] + ' '
-ifAdv = random.randrange(0,2)
-# add an adverb only sometimes
-if ifAdv == 0:
-    num = random.randrange(0,len(adv))
-    random_sentance += adv[num]
-else:
-    random_sentance += '.'
+                # tweet the sentence
+                twitter.tweet(random_sentance)
+                # wait a minute then go again
+                time.sleep(60)
+        else:
+            for i in range(x):
+                random_sentance = '@realDonaldTrump '
+
+                # add the first noun which is always a person
+                num = random.randrange(0, len(firstnouns))
+                random_sentance += firstnouns[num] + ' '
+                # what is this person doing?
+                num = random.randrange(0,len(verbs))
+                random_sentance += verbs[num] + ' '
+                # and to what/who is this person doing that thing?
+                num = random.randrange(0,len(secondnoun))
+                random_sentance += secondnoun[num] + ' '
+                ifAdv = random.randrange(0,2)
+                # add an adverb only sometimes
+                if ifAdv == 0:
+                    num = random.randrange(0,len(adv))
+                    random_sentance += adv[num]
+                else:
+                    random_sentance += '.'
+
+                # tweet the sentence
+                twitter.tweet(random_sentance)
+                print (x, 'times tweeted')
+                # wait a minute then go again
+                time.sleep(60)
         
 
 if __name__ == "__main__":
     twitter = TwitterAPI()
-    twitter.tweet(random_sentance)
+    twitter.trumpTrumpXTimes(2)
