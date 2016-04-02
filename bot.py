1958cecd548d1d812925c08b65378bd58c0d9937
Kabir Virji <kabirvirji@gmail.com>
2016-04-02 15:43:52 -0400

    key tokens

--- bot.py
+++ bot.py
@@ -1,18 +1,28 @@
 import tweepy
+import random
 
 class TwitterAPI:
     def __init__(self):
-        consumer_key = ""
-        consumer_secret = ""
+        consumer_key = "WhLHy9vrJkUcUxwk4yMzVCBHv"
+        consumer_secret = "vMFE8YA3FjeNhhVRiyy4agCjxQexHKjzXyJC7AIWrIkc0FWrLs"
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        access_token = ""
-        access_token_secret = ""
+        access_token = "716345350626394112-7ueuLKPYAMTYzZ14Of0iusdaqB59ATs"
+        access_token_secret = "0tgfufNwO0GOfQt4KBS7ivhIVfDYvGKqMyl50u4pWU8Vh"
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
 
     def tweet(self, message):
         self.api.update_status(status=message)
 
+nouns = ("puppy", "car", "rabbit", "girl", "monkey")
+verbs = ("runs", "hits", "jumps", "drives", "barfs") 
+adv = ("crazily.", "dutifully.", "foolishly.", "merrily.", "occasionally.")
+adj = ("adorable", "clueless", "dirty", "odd", "stupid")
+num = random.randrange(0,5)
+
+random_sentance = '@DonaldJTrump' + nouns[num] + ' ' + verbs[num] + ' ' + adv[num] + ' ' + adj[num]
+        
+
 if __name__ == "__main__":
     twitter = TwitterAPI()
-    twitter.tweet("I'm posting a tweet!")
+    twitter.tweet(random_sentance)
